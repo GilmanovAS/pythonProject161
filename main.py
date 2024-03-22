3101f4d6c5e983d9c5f5fc5fe0295997e8bf684c
GilmanovAS <70641821+GilmanovAS@users.noreply.github.com>
2024-03-22 07:09:01 +0500

    3

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_, desc, func
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -10,56 +12,100 @@ app.config['SECRET_KEY'] = 'DDDFSLF'
 db = SQLAlchemy(app)
 
 
-@app.route('/user/count')
-def get_user_count():
-    user_count = User.query.count()
-    return json.dumps(user_count)
-
-
-@app.route('/user/first')
-def get_user_first():
-    user_first = User.query.first()
-    return json.dumps({"id": user_first.id,
-                       "name": user_first.name})
-
-
-@app.route('/user/<int:pk>')
-def get_pk(pk: int):
-    idd = User.query.get(pk)
-    return json.dumps({"id": idd.id,
-                       "name": idd.name})
-
-
-@app.route('/user/all')
-def get_all():
-    all_user = User.query.all()
-    response = []
-    for user in all_user:
-        response.append({"id": user.id,
-                         "name": user.name}
-                        )
-    return response
-
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    age = db.Column(db.Integer, db.CheckConstraint('age>18'))
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+    group = relationship('Group')
+
+
+class Group(db.Model):
+    __tablename__ = 'group'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    users = relationship('User')
+
+
+@app.route("/users/first")
+def get_user_first():
+    ret = User.query.first()
+    return json.dumps({'id': ret.id, 'name': ret.name, 'group': ret.group.name})
+
+
+@app.route('/users/all')
+def get_user_all():
+    all_users = User.query.all()
+    temp = []
+    for user in all_users:
+        temp.append({'id': user.id, 'name': user.name, 'group': user.group.name})
+    return json.dumps(temp)
+
+
+@app.route('/users/count')
+def get_count():
+    count1 = User.query.count()
+    return json.dumps(count1)
+
+
+@app.route('/users/<int:id>')
+def get_user_pk(id: int):
+    pk = User.query.get(id)
+    return json.dumps({'id': pk.id, 'name': pk.name, 'group': pk.group.name})
 
 
 if __name__ == '__main__':
     with app.app_context():
-        # db.drop_all()
+        db.drop_all()
         db.create_all()
-        d1 = User(name='Albert2')
+        group01 = Group(name='Group01')
+        d1 = User(name='Albert', age=19, group=group01)
+
+        d2 = User(name='Azaliya', age=19)
+        d3 = User(name='Gulcvbcx', age=21)
+        d4 = User(name='Azaliya2', age=19)
+        group02 = Group(name='Group02', users=[d2, d3, d4])
+
         db.session.add(d1)
+        db.session.add(group02)
         db.session.commit()
-        d2 = User(name='Ameliya')
-        d3 = User(name='Azaliya')
-        db.session.add_all([d2, d3])
-        db.session.commit()
+        # d2 = User(name='Ameliya')
+        # d3 = User(name='Azaliya')
+        # # d4 = User()
+        # db.session.add_all([d2, d3])
+        # db.session.commit()
+        temp = User.query.filter(User.name == 'Albert')
+        print(temp)
+        print(temp.first().name)
+        # and
+        temp = User.query.filter(User.name == 'Albert', User.age == 19)
+        print(temp)
+        print(temp.all())
+        # like
+        temp = User.query.filter(User.name.like('A%'))
+        print(temp)
+        print(temp.all())
+    #   or_
+        temp = User.query.filter(or_( User.name == 'Albert', User.age == 21))
+        print(temp)
+        print(temp.all())
+        #   none
+        temp = User.query.filter(User.name == None )
+        print(temp)
+        print(temp.all())
+        #   none
+        temp = User.query.filter(User.name != None )
+        print(temp)
+        print(temp.all())
+        #   in_
+        temp = User.query.filter(or_(User.name == None ))
+        print(temp)
+        print(temp.all())
 
     app.run(debug=True)
 
-    # from flask import Flask
+# from flask import Flask
 # from flask_sqlalchemy import SQLAlchemy
 # import sqlalchemy
 #
