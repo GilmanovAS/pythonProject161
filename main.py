08d94c6f803ef886cbb19c3633d9a01003aa1cd9
GilmanovAS <70641821+GilmanovAS@users.noreply.github.com>
2024-04-01 07:13:20 +0500

    5

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_
 from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
@@ -15,7 +16,7 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tabelname__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False, unique=True)
+    name = db.Column(db.String(100), nullable=False)
     age = db.Column(db.Integer, db.CheckConstraint('age>18'))
     group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
     group = relationship('Group')
@@ -24,7 +25,7 @@ class User(db.Model):
 class Group(db.Model):
     __tablename__ = 'group'
     id = db.Column(db.Integer, primary_key=True)
-    group_name = db.Column(db.String(200), nullable=False, unique=True)
+    group_name = db.Column(db.String(200), nullable=False)
     users = relationship('User')
 
 
@@ -38,7 +39,7 @@ def user_all():
 
 
 @app.route('/user/first')
-def user_all():
+def user_first():
     tm = User.query.first()
     return json.dumps({'id': tm.id, 'name': tm.name, 'group': tm.group.group_name})
 
@@ -55,23 +56,202 @@ def user_get(id: int):
     return json.dumps({'id': temp.id, 'name': temp.name, 'group': temp.group.group_name})
 
 
+@app.route('/user/<name>/and/<int:age>')
+def op_and(name: str, age: int):
+    temp = User.query.filter(User.name == name, User.age == age)
+    print(temp)
+    print(temp.all())
+    return temp.first().group.group_name
+
+
+@app.route('/user/<name>/or/<int:age>')
+def op_or(name: str, age: int):
+    temp = User.query.filter(or_(User.name == name, User.age == age))
+    print(temp)
+    print(temp.all())
+    return temp.first().group.group_name
+
+
+@app.route('/user/<int:age1>/in/<int:age2>')
+def op_in(age1: int, age2: int):
+    temp = User.query.filter((User.age.in_([age1, age2])))
+    print(temp)
+    all_u = []
+    for _ in temp.all():
+        all_u.append({'id': _.id, 'name': _.name, 'age': _.age, 'group': _.group.group_name})
+    print(all_u)
+    return json.dumps(all_u)
+
+
+@app.route('/user/<int:age1>/notin/<int:age2>')
+def op_notin(age1: int, age2: int):
+    temp = User.query.filter((User.age.notin_([age1, age2])))
+    print(temp)
+    all_u = []
+    for _ in temp.all():
+        all_u.append({'id': _.id, 'name': _.name, 'age': _.age, 'group': _.group.group_name})
+    print(all_u)
+    return json.dumps(all_u)
+
+
+@app.route('/user/<int:age1>/between/<int:age2>')
+def op_between(age1: int, age2: int):
+    temp = User.query.filter((User.age.between(age1, age2)))
+    print(temp)
+    all_u = []
+    for _ in temp.all():
+        all_u.append({'id': _.id, 'name': _.name, 'age': _.age, 'group': _.group.group_name})
+    print(all_u)
+    return json.dumps(all_u)
+
+
+@app.route('/user/like/<like_>')
+def op_like(like_: str):
+    temp = User.query.filter((User.name.like(like_ + '%')))
+    print(temp)
+    all_u = []
+    for _ in temp.all():
+        all_u.append({'id': _.id, 'name': _.name, 'age': _.age, 'group': _.group.group_name})
+    print(all_u)
+    return json.dumps(all_u)
+
+
+@app.route('/user/none/')
+def op_none():
+    temp = User.query.filter((User.name == None))
+    print(temp)
+    all_u = []
+    for _ in temp.all():
+        all_u.append({'id': _.id, 'name': _.name, 'age': _.age, 'group': _.group.group_name})
+    print(all_u)
+    return json.dumps(all_u)
+
+
+@app.route('/user/isnone/')
+def op_isnone():
+    temp = User.query.filter((User.name != None))
+    print(temp)
+    all_u = []
+    for _ in temp.all():
+        all_u.append({'id': _.id, 'name': _.name, 'age': _.age, 'group': _.group.group_name})
+    print(all_u)
+    return json.dumps(all_u)
+
+
+@app.route('/user/update/<name>/')
+def op_update(name: str):
+    # temp = User.query.filter(User.name == name)
+    temp = User.query.first()
+    print(temp)
+    temp2 = {'id': temp.id, 'name': temp.name, 'age': temp.age, 'group': temp.group.group_name}
+    print(temp2)
+    temp.name = 'DDDDDD'
+    db.session.add(temp)
+    db.session.commit()
+    temp = User.query.get(temp.id)
+    print(temp)
+    temp2 = {'id': temp.id, 'name': temp.name, 'age': temp.age, 'group': temp.group.group_name}
+    print(temp2)
+    return json.dumps(temp2)
+
+
+@app.route('/user/delete/<name>/')
+def op_delete(name: str):
+    temp = User.query.filter(User.name == name)
+    print(temp)
+    temp = temp.query.first()
+    print(temp)
+    temp2 = {'id': temp.id, 'name': temp.name, 'age': temp.age, 'group': temp.group.group_name}
+    print(temp2)
+    db.session.delete(temp)
+    db.session.commit()
+    temp = User.query.all()
+    print(temp)
+    return json.dumps('OK')
+
+
+#
+# @app.route('/user/and/<int:age>')
+# def op_and(age: int):
+#     temp = User.query.filter(User.name == 'Azaliya841', User.age == age)
+#     print(temp)
+#     print(temp.all())
+#     return temp.first().name
+#
+#
+# @app.route('/user/like/<tex>')
+# def op_like(tex):
+#     temp = User.query.filter(User.name.like(tex + '%'))
+#     print(temp)
+#     print(temp.all())
+#     return temp.first().name
+#
+#
+# @app.route('/user/or/<int:age>')
+# def op_or(age: int):
+#     temp = User.query.filter(or_(User.name == 'Azaliya841', User.age == age))
+#     print(temp)
+#     print(temp.all())
+#     return temp.first().name
+#
+#
+# @app.route('/user/in/<int:age>')
+# def op_in(age: int):
+#     temp = User.query.filter(User.age.in_([18, age]))
+#     print(temp)
+#     print(temp.all())
+#     return temp.first().name
+#
+#
+# @app.route('/user/notin/<int:age>')
+# def op_notin(age: int):
+#     temp = User.query.filter(User.age.notin_([18, age]))
+#     print(temp)
+#     print(temp.all())
+#     return temp.first().name
+#
+#
+# @app.route('/user/null')
+# def op_null():
+#     temp = User.query.filter(User.age == None)
+#     print(temp)
+#     print(temp.all())
+#     return temp.first().name
+#
+#
+# @app.route('/user/notnull')
+# def op_notnull():
+#     temp = User.query.filter(User.age != None)
+#     print(temp)
+#     print(temp.all())
+#     return temp.first().name
+#
+#
+# @app.route('/user/between')
+# def op_between():
+#     temp = User.query.filter(User.age.between(20,21))
+#     print(temp)
+#     print(temp.all())
+#     return temp.first().name
+
+
 if __name__ == '__main__':
     with app.app_context():
         # db.drop_all()
         db.create_all()
-        g1 = Group(group_name='g1' + str(random.randint(1, 1000)))
-        name = 'Azaliya' + str(random.randint(1, 1000))
-        p1 = User(name=name, group=g1)
-        name = 'Ameliya' + str(random.randint(1, 1000))
-        p2 = User(name=name)
-        name = 'Ameliya' + str(random.randint(1, 1000))
-        p3 = User(name=name)
-        g2 = Group(group_name='g1' + str(random.randint(1, 1000)), users=[p2, p3])
+        g1 = Group(group_name='g1')
+        name = 'Azaliya2' + str(random.randint(1, 1000))
+        p1 = User(name=name, age=random.randint(19, 25), group=g1)
+        name = 'Ameliya2' + str(random.randint(1, 1000))
+        p2 = User(name=name, age=random.randint(19, 25))
+        name = 'Ameliya2' + str(random.randint(1, 1000))
+        p3 = User(name=name, age=random.randint(19, 25))
+        g2 = Group(group_name='g2', users=[p2, p3])
 
         with db.session.begin():
             db.session.add(p1)
             db.session.add(g2)
-        print(User.query.filter(User.name == 'Azaliya270').first().name)
+        # print(User.query.filter(User.name == 'Azaliya2391').first().name)
         # db.session.commit()
 
     app.run(debug=True)
